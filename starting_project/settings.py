b77318e1ea8430acfd6456a0404a25d04bd9b0c3
kush2487 <107868296+kush2487@users.noreply.github.com>
2025-05-02 17:07:38 +0530

    Update settings.py

--- starting_project/settings.py
+++ starting_project/settings.py
@@ -83,19 +83,9 @@ WSGI_APPLICATION = 'starting_project.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
-
-
-        'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'Ecommerce',
-        'USER': 'product',
-        'PASSWORD': 'KUSHagra',
-        'HOST': 'database-1.c3csy4awux2w.ap-south-1.rds.amazonaws.com',
-        'PORT': '3306',
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
